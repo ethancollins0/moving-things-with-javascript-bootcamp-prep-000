2e93babee03e0b20589ec7ee1fd321d16d5e1984
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 04:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-    dodger.style.right = `${right - 1}px`
-    }
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
