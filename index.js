01604d9de2a014b73cffc2b67540fb206b2b134a
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 04:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.addEventListener('keydown', function(e) {
   }
 })
 
-document.addEventListener('keydown', function(e) {
+//document.addEventListener('keydown', function(e) {
   if (e.which === 39) {
     moveDodgerRight()
   }
