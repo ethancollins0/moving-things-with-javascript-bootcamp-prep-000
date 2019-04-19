eb124ebbce09948ce3597f8b11a2e3c1b7f0e245
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 04:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 
 function moveDodgerLeft() {
-document.addEventListener('keydown', function(e) {
+  document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
   }
