9169afcc0feac2e47ede64585dd09e6d5c0c52c5
Ethan Collins <ethantcollins98@gmail.com>
2019-04-19 04:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
+dodger.style.right = '0px'
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
